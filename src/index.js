947a1f855c233e6de1048d5f9b7c93bdce807df5
OlegRudevich <olegrudevich19@gmail.com>
2019-10-13 11:32:06 +0300

    init : add decision.

---
+++ src/index.js
@@ -0,0 +1,18 @@
+/**
+ * @param preferences - target student focus
+ * @param knowsProgramming - if student can do programming and know basics
+ * @param config - private student ability to perform for different focus modes
+ * @returns number of weeks needed for finish education
+ */
+module.exports = function getTimeForEducation(
+    focus = 'family', 
+    knowsProgramming = true,
+    config = {family: 4}
+    ) {
+
+		
+		console.log('configffffff',config[focus]);
+
+      return knowsProgramming ?  Math.ceil(800 / config[focus])  : Math.ceil(1300 / config[focus]);
+  };
+  
